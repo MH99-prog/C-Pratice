ad42287c0eb5e192cd7857e559583e23c81c71a3
Muhammad Haleem <33144659+Rackerthomas@users.noreply.github.com>
2019-11-08 23:44:15 +0500

    Add files via upload
    Program.cs AND Program_2.cs

---
+++ C# practice/Program.cs
@@ -0,0 +1,88 @@
+﻿/****HELLO  WORLD****/
+
+//using System;
+
+
+  // class HelloWorld {
+ //     static void Main(string[] args) {
+         /* my first program in C# */
+ //        Console.WriteLine("Hello World");
+        
+ //     }
+  // }
+
+
+
+  //***Taking User input***// 
+
+
+  //using System;
+
+//class program 
+//{
+//    static void Main()
+    //{
+      //  Console.WriteLine("Please enter your name");
+        //string Username  = Console.ReadLine();
+        //Console.WriteLine("Helle "+Username);
+//    }
+//}
+
+
+//using System;
+
+
+ //   class program{
+          //  static void Main()
+ //            {
+ //              int i   = 44;
+ //              int j = 44;
+ //              Console.WriteLine(i+j);
+ //            }
+ //    }
+
+
+// DIVISION  PROGRAM //
+   // ******* //
+   //  ****   //
+   //   **    //
+   //   **  //
+//using System;
+//
+//class program{
+//    static void Main()
+//    {
+//        int Numerator = 22;
+//        int Denomiator = 10;
+//
+//        int Result = Numerator % Denomiator;
+//
+//        Console.WriteLine("Result = {0}", Result);
+//    }
+//}
+
+
+
+// IF--ELSE  STATEMENT  PROGRAM //
+
+
+//using System;
+
+//class program
+//{
+//    static void Main()
+//    {
+//            int Number = 120;
+//            int AnotherNumber = 2;
+//            
+//            if (Number ==10 || AnotherNumber == 20)
+//
+//            Console.WriteLine("Hello");
+//
+//            else
+//            Console.WriteLine("not correct");
+//    }
+//
+//}
+
+
---
+++ C# practice/Program_2.cs
@@ -0,0 +1,171 @@
+//BOOLEAN  CONCEPTS//
+
+//using System;
+//
+//class program
+//{
+//    static void Main()
+//    {
+//        bool? Areyoumajor = null;
+//        if (Areyoumajor == true)
+//        {
+//        Console.WriteLine("your are major");
+//        }
+//            else if (Areyoumajor ==false)
+//            {
+//                Console.WriteLine("User is not major");
+//            }
+//
+//            else{
+//                Console.WriteLine("User not available");
+//            }
+//    }
+//}
+
+
+
+//Datatype Conversion//
+
+//using System;
+//class program{
+//    static void Main()
+//    {
+//        float f = 123.43F;
+//        int j = (int)f;     // float_type conversion to integer_type//
+//                
+//        string strNumber = "100";
+//        int i = int.Parse(strNumber);
+//        Console.WriteLine(i);
+//        
+//    }
+//}
+
+
+//ARRAYS/////////////////////////////////////////
+//using System;
+//
+//class program{
+//    static void Main()
+//    {
+//        int[] Evennumbers = new int[3];
+//
+//        Evennumbers [0] = 2;
+//        Evennumbers [1] = 4;
+//        Evennumbers [2] = 6;
+//        Console.WriteLine(Evennumbers[0]);
+//
+//    }
+//}
+
+
+/////////////////////////////     IF-ELSE STATEMENT     ///////////////////////////
+
+
+
+//using System;
+//class  Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Enter a number");
+//
+//        int UserNumber  = int.Parse(Console.ReadLine());
+//
+//        if (UserNumber == 1)
+//        {
+//            Console.WriteLine("Your age is {0}",UserNumber);
+//        }
+//
+//        else if (UserNumber == 2)
+//        {
+//            Console.WriteLine("your number is {0}", UserNumber);
+//
+//        }
+//
+//        else if (UserNumber == 3)
+//        {
+//            Console.WriteLine("your number is {0} ", UserNumber);
+//        }
+//
+//      
+//
+//        else 
+//        {
+//            Console .WriteLine("Your number is in correct");
+//        }
+//    }
+//}
+//
+//using System;
+//
+//class program 
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Please enter a number");
+//
+//        int UserNumber = int.Parse(Console.ReadLine());
+//
+//        if (UserNumber == 10 || UserNumber == 20)
+//        {
+//            Console.WriteLine("your is 10 or 20");
+//        }
+//        else 
+//        {
+//            Console.WriteLine("Your number is not specified");
+//        }
+//    }
+//}
+
+ /////////////////////////////SWITCH--STATEMENT/////////////////////////
+
+
+
+// 
+//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//       Console.WriteLine("Please enter a given fruit");
+//      
+//      string str = Console.ReadLine();
+//
+//     
+//
+//        
+//       switch (str)
+//       {
+//           case "Grapes":
+//           Console.WriteLine("You have chosen a correct fruit");
+//           break;
+//
+//           case "Apple":
+//           Console.WriteLine("You have chosen a correct fruit");
+//           break;
+//
+//           case "strawbery":
+//           Console.WriteLine("You have chosen a correct fruit");
+//           break;
+//
+//           default:
+//           Console.WriteLine("Fruit is not available");
+//
+//           break;
+//
+//
+//       }
+//
+//    }
+//}
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
